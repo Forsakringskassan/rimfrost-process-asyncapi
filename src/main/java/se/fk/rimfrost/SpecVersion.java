dc4e34aae8ba5c8a6b4bb77d0a2e1e440f09a562
Nils Elveros <nils.elveros@tromb.com>
2026-06-02 13:31:54 +0200

    fix: specverison as 1.0 instead of v1

--- src/main/java/se/fk/rimfrost/SpecVersion.java
+++ src/main/java/se/fk/rimfrost/SpecVersion.java
@@ -2,5 +2,37 @@ package se.fk.rimfrost;
 
 public enum SpecVersion
 {
-   V1
+   V1("1.0")
+
+   private String value;
+
+   private SpecVersion(String value)
+   {
+      this.value = value;
+   }
+
+   @JsonValue
+   public String getValue()
+   {
+      return this.value;
+   }
+
+   @JsonCreator
+   public static SpecVersion fromValue(String value)
+   {
+      for (SpecVersion e : values())
+      {
+         if (e.value.equals(value))
+         {
+            return e;
+         }
+      }
+
+      throw new IllegalArgumentException("Unexpected value '" + value + "'");
+   }
+
+   public String toString()
+   {
+      return String.valueOf(this.value);
+   }
 }
